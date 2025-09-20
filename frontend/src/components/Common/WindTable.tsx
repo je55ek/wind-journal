d915e513b931328fa3512bb721336c10b9fa12e1
Jesse Kelly <jessemkelly@gmail.com>
2025-09-20 13:34:23 -0500

    New session component first draft done

--- frontend/src/components/Common/WindTable.tsx
+++ frontend/src/components/Common/WindTable.tsx
@@ -3,24 +3,30 @@ import { For, Stack } from "@chakra-ui/react"
 import WindTableColumn from "@/components/Common/WindTableColumn"
 
 interface WindTableProps {
-    firstHour: number,
-    lastHour: number
+  firstHour: number
+  lastHour: number
 }
 
 function WindTable({ firstHour, lastHour }: WindTableProps) {
-    return !(isNaN(firstHour) || isNaN(lastHour)) && (lastHour >= firstHour) ? (
-        <Stack direction="row" gap="1px">
-            <For each={Array.from({ length: lastHour - firstHour + 1 }, (_, i) => i + firstHour)}>
-                {(hour) => <WindTableColumn hour={hour} />}
-            </For>
-        </Stack>
-    ) : (
-        <Stack direction="row" gap="1px">
-            <WindTableColumn hour={undefined} />
-            <WindTableColumn hour={undefined} />
-            <WindTableColumn hour={undefined} />
-        </Stack>
-    )
+  return !(Number.isNaN(firstHour) || Number.isNaN(lastHour)) &&
+    lastHour >= firstHour ? (
+    <Stack direction="row" gap="1px">
+      <For
+        each={Array.from(
+          { length: lastHour - firstHour + 1 },
+          (_, i) => i + firstHour,
+        )}
+      >
+        {(hour) => <WindTableColumn hour={hour} />}
+      </For>
+    </Stack>
+  ) : (
+    <Stack direction="row" gap="1px">
+      <WindTableColumn hour={undefined} />
+      <WindTableColumn hour={undefined} />
+      <WindTableColumn hour={undefined} />
+    </Stack>
+  )
 }
 
 export default WindTable
--- frontend/src/components/Common/WindTableColumn.tsx
+++ frontend/src/components/Common/WindTableColumn.tsx
@@ -1,8 +1,27 @@
-import { Box, Editable, IconButton, Portal, Select, Skeleton, Stack, createListCollection, useSelectContext, useEditableContext } from "@chakra-ui/react"
-import { ReactNode } from "react"
-import { MdNorth, MdSouth, MdEast, MdWest } from "react-icons/md"
+import {
+  Box,
+  Editable,
+  IconButton,
+  Portal,
+  Select,
+  Skeleton,
+  Stack,
+  createListCollection,
+  useEditableContext,
+  useSelectContext,
+} from "@chakra-ui/react"
+import { type ReactNode, useState } from "react"
+import {
+  MdEast,
+  MdNorth,
+  MdNorthEast,
+  MdNorthWest,
+  MdSouth,
+  MdSouthEast,
+  MdSouthWest,
+  MdWest,
+} from "react-icons/md"
 import { RiForbidLine } from "react-icons/ri"
-
 
 interface WindTableColumnProps {
   hour: number
@@ -11,11 +30,39 @@ interface WindTableColumnProps {
 const directions = createListCollection({
   items: [
     { value: "N", icon: <MdNorth /> },
+    { value: "NE", icon: <MdNorthEast /> },
     { value: "E", icon: <MdEast /> },
+    { value: "SE", icon: <MdSouthEast /> },
     { value: "S", icon: <MdSouth /> },
-    { value: "W", icon: <MdWest /> }
-  ]
+    { value: "SW", icon: <MdSouthWest /> },
+    { value: "W", icon: <MdWest /> },
+    { value: "NW", icon: <MdNorthWest /> },
+  ],
 })
+
+function cellBackgroundColor(value: string) {
+  const kts = Math.max(0, Number.parseFloat(value))
+  const stops = [
+    { max: 0.0, color: "var(--chakra-colors-blue-700)" },
+    { max: 5.8, color: "var(--chakra-colors-blue-600)" },
+    { max: 9.7, color: "var(--chakra-colors-blue-400)" },
+    { max: 12.0, color: "var(--chakra-colors-green-400)" },
+    { max: 19.0, color: "var(--chakra-colors-yellow-400)" },
+    { max: 29.0, color: "var(--chakra-colors-red-500)" },
+    { max: 78.0, color: "var(--chakra-colors-purple-700)" },
+    { max: Number.POSITIVE_INFINITY, color: "var(--chakra-colors-purple-700)" },
+  ]
+  if (Number.isNaN(kts)) {
+    return "var(--chakra-colors-gray-100)"
+  }
+
+  const i = stops.findIndex((s) => kts < s.max)
+  const [c1, c2] = stops.slice(i - 1, i + 1)
+
+  return `color-mix(in hsl decreasing hue, ${c1.color}, ${c2.color} ${
+    ((kts - c1.max) / (c2.max - c1.max)) * 100
+  }%)`
+}
 
 interface WindTableDirection {
   value: string
@@ -30,7 +77,8 @@ const WindTableDirectionTrigger = () => {
       w="full"
       rounded="0"
       size="xs"
-      bg="navy"
+      maxH="6"
+      bg="blue.800"
       {...select.getTriggerProps()}
     >
       {select.hasSelectedItems ? items[0].icon : <RiForbidLine />}
@@ -39,24 +87,34 @@ const WindTableDirectionTrigger = () => {
 }
 
 function WindTableColumn({ hour }: WindTableColumnProps) {
-  const loading=isNaN(hour)
-  const hour_12 = (Math.round(hour) + 11) % 12 + 1
+  const [avg, setAvg] = useState("")
+  const [gust, setGust] = useState("")
+  const loading = Number.isNaN(hour)
+  const hour_12 = ((Math.round(hour) + 11) % 12) + 1
 
   return (
     <Skeleton asChild loading={loading}>
       <Stack direction="column" align="flex-start" w="10" gap="0">
         <Stack bg="ui.main" color="white" w="full" gap="0">
-            <Box textAlign="center" >{loading ? "00" : hour_12.toString().padStart(2, "0")}</Box>
-            <Box fontSize="xs" textAlign="center">{loading ? "XX" : (hour < 13 ? "AM" : "PM")} </Box>
+          <Box textAlign="center">
+            {loading ? "00" : hour_12.toString().padStart(2, "0")}
+          </Box>
+          <Box fontSize="xs" textAlign="center">
+            {loading ? "XX" : hour < 13 ? "AM" : "PM"}{" "}
+          </Box>
         </Stack>
         <Editable.Root
           key="average"
           defaultValue=""
           placeholder="avg"
           w="full"
-          bg="gray.100"
+          css={{
+            background: cellBackgroundColor(avg),
+          }}
           textAlign="center"
           justifyContent="center"
+          value={avg}
+          onValueChange={(e) => setAvg(e.value)}
         >
           <Editable.Preview />
           <Editable.Input rounded="0" />
@@ -66,9 +124,13 @@ function WindTableColumn({ hour }: WindTableColumnProps) {
           defaultValue=""
           placeholder="gust"
           w="full"
-          bg="gray.100"
+          css={{
+            background: cellBackgroundColor(gust),
+          }}
           textAlign="center"
           justifyContent="center"
+          value={gust}
+          onValueChange={(e) => setGust(e.value)}
         >
           <Editable.Preview />
           <Editable.Input rounded="0" />
@@ -80,9 +142,15 @@ function WindTableColumn({ hour }: WindTableColumnProps) {
           </Select.Control>
           <Portal>
             <Select.Positioner>
-              <Select.Content bg="navy" color="white" p="0" m="0" rounded="0">
+              <Select.Content bg="blue.800" color="white" p="0" rounded="0">
                 {directions.items.map((item) => (
-                  <Select.Item key={item.value} item={item}>
+                  <Select.Item
+                    key={item.value}
+                    item={item}
+                    rounded="0"
+                    justifyContent="center"
+                    maxH="6"
+                  >
                     {item.icon}
                   </Select.Item>
                 ))}
